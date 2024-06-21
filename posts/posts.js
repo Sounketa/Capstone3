a98f3acabffe86199bb92bd019ad1bc6a18450a7
Sounketa <julien.ilboudo97@gmail.com>
2024-06-21 18:08:20 -0400

    Made changes and added functionality to post page

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,33 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+
+// GET http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts/
+// Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imp1bGllbiIsImlhdCI6MTcxODk4MzUwMCwiZXhwIjoxNzE5MDY5OTAwfQ.CDYPBsEsqMTNMHXoyNX3vjmGq6BYRG439hAqcHcXVrs
+
+/* Posts Page JavaScript */
+
+"use strict";
+
+const postsContainer = document.querySelector("#posts-container");
+
+fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts/", {
+  method: "GET",
+  headers: {
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imp1bGllbiIsImlhdCI6MTcxODk4MzUwMCwiZXhwIjoxNzE5MDY5OTAwfQ.CDYPBsEsqMTNMHXoyNX3vjmGq6BYRG439hAqcHcXVrs"
+  }
+})
+.then(response => response.json())
+.then(data => {
+  data.forEach(post => {
+    const postHTML = `
+      <div class="post">
+        <span class="post-author">${post.author}</span>
+        <p>${post.content}</p>
+      </div>
+    `;
+    postsContainer.innerHTML += postHTML;
+  });
+})
+.catch(error => console.error("Error fetching posts:", error));
